498f0195206e28b44e20cdc9ca07e9181e30014b
Ying Hong <ying_hong@trimble.com>
2017-02-12 21:51:06 -0700

    Made it around track 1 multiple times

--- model.py
+++ model.py
@@ -93,6 +93,8 @@ model = Sequential([
 	
 ])
 
+print ("Model defined")
+
 #
 # Model Compilation
 #
@@ -100,7 +102,9 @@ model = Sequential([
 learning_rate = 0.001
 # for a mean squared error regression problem
 adam = Adam(lr = learning_rate)
-model.compile(optimizer = adam, loss = 'mean_squared_error')
+model.compile(optimizer = adam, loss = 'mean_squared_error', metrics=['mean_absolute_percentage_error'])
+
+print ("Model compiled")
 
 #
 # Model Diagram
@@ -113,11 +117,13 @@ model.compile(optimizer = adam, loss = 'mean_squared_error')
 from keras.utils.visualize_util import plot
 plot(model, to_file='model.png', show_shapes=True, show_layer_names=True)
 
+print("Model diagram")
+
 #
 # Model Training
 #
 
-# Start with example from "How to Use Generators"
+### Start with example from "How to Use Generators"
 import os
 import csv
 
@@ -130,14 +136,18 @@ with open('../ud-sim-data/driving_log.csv') as csvfile:
 # remove headings row
 samples.pop(0)
 
+print("Samples CSV loaded len={}".format(len(samples)))
+
 # shuffle and split into train and validation sets
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2, random_state=88)
 
+print("Samples split train size={}, validation size={}".format(len(train_samples), len(validation_samples)))
+
 # image plot library
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-%matplotlib
+# %matplotlib # uncomment for ipython
 
 import cv2
 import numpy as np
@@ -145,46 +155,92 @@ import sklearn
 import PIL
 from scipy.ndimage.interpolation import zoom
 
-def generator(samples, batch_size=32, images_dir = '../ud-sim-data/'):
+### Set up generator
+print("Generator setup ...")
+
+def generator(samples, batch_size=32, images_dir = '../ud-sim-data/', left_right=False, flip=False):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
+            
             images = []
             angles = []
+            angle_adjust = 0.15 # amount of correction for left and right cameras
+            center_index = 0
+            left_index=1
+            right_index=2
+            angle_index=3
+            
             for batch_sample in batch_samples:
                 # name = './IMG/'+batch_sample[0].split('/')[-1]
                 # center_image = cv2.imread(name) 
-				# use PIL instead to read as RGB not cv2 BGR? as drive.py uses PIL
-                center_image = PIL.Image.open( images_dir + batch_sample[0] )
-                center_angle = float(batch_sample[3])
+                # use PIL instead to read as RGB not cv2 BGR? as drive.py uses PIL
+                center_image = PIL.Image.open( images_dir + batch_sample[center_index] )
+                center_angle = float(batch_sample[angle_index])
                 images.append( np.asarray( center_image ) )
                 angles.append(center_angle)
-			
+                
+                # add in left and right images
+                if left_right:
+                    # left image, adjust angle to center
+                    left_image = PIL.Image.open( images_dir + batch_sample[ left_index ].strip() )
+                    left_angle = float(batch_sample[angle_index]) + angle_adjust # if car on left side, want to go more right
+                    images.append( np.asarray( left_image ) )
+                    angles.append( left_angle )
+                    
+                    # right image, adjust angle to center
+                    right_image = PIL.Image.open( images_dir + batch_sample[ right_index ].strip() )
+                    right_angle = float(batch_sample[angle_index]) - angle_adjust # if car on right side, want to go more left
+                    images.append( np.asarray( right_image ) )
+                    angles.append( right_angle )
+                    
+                # flip image vertically
+                if flip:
+                    image_flipped = np.fliplr(center_image)
+                    angle_flipped = -center_angle
+                    images.append( np.asarray( image_flipped ) )
+                    angles.append( angle_flipped )
+                
             X_train = np.array(images)
             y_train = np.array(angles)
-			
-			# crop images to remove upper horizontal band above horizon (road) and lower horizontal band that includes the hood of car
+            
+            # crop images to remove upper horizontal band above horizon (road) and lower horizontal band that includes the hood of car
             X_train = X_train[:, 60:124, 0:320, :]
-			
-			# resize to 64x64
-			# X_resized = [cv2.resize(img,(64, 64), interpolation = cv2.INTER_CUBIC) for img in X]
-			#http://stackoverflow.com/questions/40201846/resizing-ndarray-of-images-efficiently
+            
+            # resize to 64x64
+            # X_resized = [cv2.resize(img,(64, 64), interpolation = cv2.INTER_CUBIC) for img in X]
+            #http://stackoverflow.com/questions/40201846/resizing-ndarray-of-images-efficiently
             X_train=zoom(X_train,zoom=(1,1,64./320,1),order=1)
-
+            
             yield sklearn.utils.shuffle(X_train, y_train)
+            
+# no left/right, no flip
+# train_generator = generator(train_samples, batch_size=32)
+# validation_generator = generator(validation_samples, batch_size=32)
 
-# compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+# flip images
+train_generator = generator(train_samples, batch_size=128, flip=True, left_right=True)
+validation_generator = generator(validation_samples, batch_size=128, flip=True, left_right=True)
 
+
+
+print("Start training ...")
+
+# *2 to add flipped images
 history = model.fit_generator(train_generator, 
-	samples_per_epoch=len(train_samples), 
-	validation_data=validation_generator, 
-	nb_val_samples=len(validation_samples), 
-	nb_epoch=10)
+    samples_per_epoch=len(train_samples)*4, 
+    validation_data=validation_generator, 
+    nb_val_samples=len(validation_samples)*4, 
+    nb_epoch=6)
+    
+# center only
+# history = model.fit_generator(train_generator, 
+#     samples_per_epoch=len(train_samples), 
+#     validation_data=validation_generator, 
+#     nb_val_samples=len(validation_samples), 
+#     nb_epoch=5)
 	
 #
 # plot history for loss
@@ -195,7 +251,7 @@ plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+# plt.show()
 
 # train the model, iterating on the data in batches
 # of 32 samples
@@ -203,3 +259,5 @@ plt.show()
 
 # Save your trained model architecture as model.h5 using model.save('model.h5').
 model.save('model.h5')
+
+print('model saved')
