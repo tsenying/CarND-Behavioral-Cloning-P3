fb3b5ce15d82b853b998ef84a23d173a359a42d7
Ying Hong <ying_hong@trimble.com>
2017-02-14 17:33:12 -0700

    adjust for ipython

--- BehavioralCloning.py
+++ BehavioralCloning.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-get_ipython().magic('matplotlib inline')
+#get_ipython().magic('matplotlib inline')
 
 
 # In[3]:
